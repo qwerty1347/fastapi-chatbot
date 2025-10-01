8109c774430aa96e5a3f1f000f90b8f1c5842931
이건희 <qwertys1347@gmail.com>
2025-10-01 17:58:11 +0900

    feat: vectorDB point 저장 추가

--- app/domain/agent/modules/vectordb/qdrant.py
+++ app/domain/agent/modules/vectordb/qdrant.py
@@ -1,4 +1,4 @@
-from qdrant_client import QdrantClient
+from qdrant_client import AsyncQdrantClient
 from sentence_transformers import SentenceTransformer
 
 from common.constants.embedding_model import EmbeddingModelConstants
@@ -7,5 +7,12 @@ from config.settings import settings
 
 class Qdrant:
     def __init__(self):
-        self.qdrant = QdrantClient(url=settings.QDRANT_HOST)
+        self.qdrant = AsyncQdrantClient(url=settings.QDRANT_HOST)
         self.embedding_model = SentenceTransformer(EmbeddingModelConstants.MODELS['HuggingFace']['name'])
+
+
+    async def upsert_points(self, points: list):
+        scroll_resp  = await self.qdrant.upsert(
+            collection_name="domeggook",
+            points=points,
+        )
--- app/domain/agent/services/vectordb_service.py
+++ app/domain/agent/services/vectordb_service.py
@@ -1,14 +1,34 @@
+import uuid
+
+from qdrant_client.http.models import PointStruct
+
 from app.domain.agent.modules.vectordb.qdrant import Qdrant
 from storage.vectordb.data.base import get_vectordb_data
 
 
 class VectorDBService:
     def __init__(self):
-        self.vectordb_data = get_vectordb_data()
-        pass
+        self.qdrant = Qdrant()
 
 
     async def upsert_points(self):
-        qdrant = Qdrant()
-        print(self.vectordb_data)
-        pass
+        points = []
+        data = get_vectordb_data()
+
+        for index, (key, value) in enumerate(data.items()):
+            for idx, row in enumerate(value):
+                for sub_idx, item in enumerate(row):
+                    points.append(
+                        PointStruct(
+                            id=str(uuid.uuid4()),
+                            vector=self.qdrant.embedding_model.encode(item['text']).tolist(),
+                            payload={
+                                "document_id": f"{key}{idx}{sub_idx}",
+                                "category": key,
+                                "paragraph": idx,
+                                "text": item['text']
+                            }
+                        )
+                    )
+
+        await self.qdrant.upsert_points(points)
