31f0e97e9f8272f201fc8409a38cc7d491a2cd2b
이건희 <qwertys1347@gmail.com>
2025-11-18 16:51:48 +0900

    docs: llm 프롬프트 개선

--- common/constants/agent/tools.py
+++ common/constants/agent/tools.py
@@ -4,9 +4,9 @@ class ToolConstants:
 
     descriptions = {
         DB_SEARCH: {
-            "description": "도매꾹과 관련된 정보가 필요할 경우 사용할 것",
+            "description": "도매꾹과 관련된 모든 정보 검색에 사용. 질문에 '도매꾹'이 포함되어 있다면 항상 DB_SEARCH 사용할 것",
         },
         WEB_SEARCH: {
-            "description": "도매꾹을 제외한 정보가 필요할 경우 사용할 것",
+            "description": "도매꾹을 제외한 일반 정보 검색에 사용할 것",
         }
     }
--- common/utils/prompt.py
+++ common/utils/prompt.py
@@ -1,6 +1,6 @@
 def set_output_prompt(user_input: str, observations: str) -> str:
     return f"""
-        아래 질문에 알맞게 적절한 답변으로 아래 규칙에 맞게 출력할 것
+        정보를 활용하여 질문에 가장 적절한 답변으로 규칙에 맞게 출력할 것
 
         규칙:
         - 맞춤법과 철자를 정확히 지킬 것
@@ -16,8 +16,13 @@ def set_output_prompt(user_input: str, observations: str) -> str:
 
 def is_chitchat_prompt(user_input: str) -> str:
     return f"""
-        아래 질문이 단순 인간의 감정 표현(인사, 고마움, 슬픔, 위로, 슬픔, 잡담 등) 일상적인 대화인지 판별할 것
-        해당하면 yes, 아니면 no 로 출력
+        질문이 단순 인간의 감정 표현(인사, 고마움, 슬픔, 위로, 잡담 등) 또는 일상적인 대화인지 판별할 것
+        질문이 정보 요청이나 특정 사안에 대한 사실 확인이면 no 만 출력할 것
+        질문이 단순 일상 대화라면 yes 만 출력할 것
+
+        규칙:
+        - 출력은 yes 또는 no 단 한 단어만 사용할 것
+        - 어떠한 설명도 추가하지 말 것
 
         질문: {user_input}
         """
@@ -25,7 +30,11 @@ def is_chitchat_prompt(user_input: str) -> str:
 
 def set_chitchat_prompt(user_input: str) -> str:
     return f"""
-        아래 질문에 맞는 적절하게 출력하고 적절한 이모지를 넣어 자연스러운 대화로 출력할 것
+        질문에 가장 적절한 답변을 자연스러운 한국어 대화로 출력하고 절대 영어를 사용하지 말 것
+        적절한 이모지를 넣어 출력할 것
+
+        예시:
+        안녕하세요! 😊 오늘 기분은 어떠세요?
 
         질문: {user_input}
         """
