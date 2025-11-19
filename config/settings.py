d110b0c927a29da9088d92d9bb88b6b7c570d407
이건희 <qwertys1347@gmail.com>
2025-11-19 17:52:58 +0900

    feat: langsmith env 환경 추가

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,13 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     ALLOWED_ORIGINS: str | None
+
     QDRANT_HOST: str
+
+    LANGCHAIN_TRACING_V2: str
+    LANGCHAIN_ENDPOINT: str
+    LANGCHAIN_API_KEY: str
+    LANGCHAIN_PROJECT: str
 
     GROQ_API_KEY: str
     SERP_API_KEY: str
